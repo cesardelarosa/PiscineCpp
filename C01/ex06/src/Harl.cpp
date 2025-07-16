7e5ad8bccd9c3a85f7d7e2a846634ac7e068a754
cde-la-r <code@cesardelarosa.xyz>
2025-07-16 22:55:42 +0200

    06

---
+++ C01/ex06/src/Harl.cpp
@@ -0,0 +1,77 @@
+#include "Harl.hpp"
+#include <iostream>
+#include <string>
+
+#define DEBUG_MSG                                                              \
+  "I love having extra bacon for my "                                          \
+  "7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+#define INFO_MSG                                                               \
+  "I cannot believe adding extra bacon costs more money. You didn’t put "    \
+  "enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+#define WARNING_MSG                                                              \
+  "I think I deserve to have some extra bacon for free. I’ve been coming for " \
+  "years, whereas you started working here just last month."
+#define ERROR_MSG "This is unacceptable! I want to speak to the manager now."
+
+void Harl::debug() {
+  std::cout
+      << "[DEBUG] "
+      << "I love having extra bacon for my "
+         "7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!"
+      << std::endl;
+}
+
+void Harl::info() {
+  std::cout
+      << "[INFO] "
+      << "I cannot believe adding extra bacon costs more money. You didn’t put "
+         "enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+      << std::endl;
+}
+
+void Harl::warning() {
+  std::cout
+      << "[WARNING] "
+      << "I think I deserve to have some extra bacon for free. I’ve been "
+         "coming for years, whereas you started working here just last month."
+      << std::endl;
+}
+
+void Harl::error() {
+  std::cout << "[ERROR] "
+            << "This is unacceptable! I want to speak to the manager now."
+            << std::endl;
+}
+
+int getLevelIndex(const std::string &level) {
+  if (level == "DEBUG") return 0;
+  if (level == "INFO") return 1;
+  if (level == "WARNING") return 2;
+  if (level == "ERROR") return 3;
+  return -1;
+}
+
+void Harl::complain(std::string level) {
+  switch (getLevelIndex(level)) {
+    case 0:
+      debug();
+      /* fallthrough */
+    case 1:
+      info();
+      /* fallthrough */
+    case 2:
+      warning();
+      /* fallthrough */
+    case 3:
+      error();
+      break;
+    default:
+      std::cout << "[ Probably complaining about insignificant problems ]"
+                << std::endl;
+      break;
+  }
+}
+
+Harl::Harl() {}
+
+Harl::~Harl() {}

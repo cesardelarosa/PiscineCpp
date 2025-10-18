af2d072ffd04bbb855c3d85e14cefaa9e9d7034a
César de la Rosa <code@cesardelarosa.xyz>
2025-10-18 15:41:58 +0200

    test

--- C03/ex00/src/main.cpp
+++ C03/ex00/src/main.cpp
@@ -2,20 +2,20 @@
 #include <iostream>
 
 int main() {
-	ClapTrap clap("CL4P-TP");
-	clap.attack("un Skag");
+	ClapTrap clap("CL4P-TR4P");
+	clap.attack("you");
 	clap.takeDamage(5);
 	clap.beRepaired(3);
 	clap.takeDamage(8);
-	clap.attack("el aire");
+	clap.attack("me");
 	clap.beRepaired(10);
 
-	ClapTrap trap("TR4P-TP");
-	trap.attack("objetivo 1");
-	trap.attack("objetivo 2");
-	trap.attack("objetivo 3");
-	trap.attack("objetivo 4");
-	trap.attack("objetivo 5");
+	ClapTrap trap("CLAP-TRAP");
+	trap.attack("target 1");
+	trap.attack("target 2");
+	trap.attack("target 3");
+	trap.attack("target 4");
+	trap.attack("target 5");
 	trap.beRepaired(2);
 	trap.beRepaired(2);
 	trap.beRepaired(2);

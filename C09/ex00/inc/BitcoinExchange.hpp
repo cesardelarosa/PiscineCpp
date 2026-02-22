5af5b36ee98723221e62e8e3f7966d1f3a25f33f
César de la Rosa <code@cesardelarosa.xyz>
2026-02-22 12:10:54 +0100

    some fts

--- C09/ex00/inc/BitcoinExchange.hpp
+++ C09/ex00/inc/BitcoinExchange.hpp
@@ -13,6 +13,12 @@ class BitcoinExchange {
 
 	void loadDatabase(const std::string &dbPath);
 	void processInput(const std::string &inputPath);
+
+  private:
+	bool isValidDate(const std::string &date) const;
+	bool isValidValue(float value) const;
+
+	std::map<std::string, float> localData;
 };
 
 #endif
--- C09/ex00/src/BitcoinExchange.cpp
+++ C09/ex00/src/BitcoinExchange.cpp
@@ -1,4 +1,7 @@
 #include "BitcoinExchange.hpp"
+#include <stdexcept>
+#include <cstdlib>
+#include <cctype>
 
 BitcoinExchange::BitcoinExchange() {}
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &src) { *this = src; }
@@ -8,5 +11,60 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &rhs) {
 }
 BitcoinExchange::~BitcoinExchange() {}
 
-void BitcoinExchange::loadDatabase(const std::string &dbPath) {}
-void BitcoinExchange::processInput(const std::string &inputPath) {}
+void BitcoinExchange::loadDatabase(const std::string &dbPath) { (void)dbPath; }
+
+void BitcoinExchange::processInput(const std::string &inputPath) {
+	(void)inputPath;
+}
+
+bool BitcoinExchange::isValidDate(const std::string &date) const {
+    // Format YYYY-MM-DD
+    if (date.length() != 10 || date[4] != '-' || date[7] != '-') {
+        throw std::invalid_argument("bad input => " + date);
+    }
+
+    // YYYY, MM & DD are numbers
+    for (size_t i = 0; i < date.length(); ++i) {
+        if (i == 4 || i == 7) continue;
+        if (!isdigit(date[i])) {
+            throw std::invalid_argument("bad input => " + date);
+        }
+    }
+
+    // Get numbers data
+    int year = std::atoi(date.substr(0, 4).c_str());
+    int month = std::atoi(date.substr(5, 2).c_str());
+    int day = std::atoi(date.substr(8, 2).c_str());
+
+    // Valid year
+    if (year < 2009) {
+        throw std::invalid_argument("bad input => " + date);
+    }
+
+	// Valid month
+    if (month < 1 || month > 12) {
+        throw std::invalid_argument("bad input => " + date);
+    }
+
+	// Valid days
+    int daysInMonth[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        daysInMonth[2] = 29;
+    }
+    if (day < 1 || day > daysInMonth[month]) {
+        throw std::invalid_argument("bad input => " + date);
+    }
+
+    return true;
+}
+
+bool BitcoinExchange::isValidValue(float value) const {
+    if (value < 0) {
+        throw std::invalid_argument("not a positive number.");
+    }
+    if (value > 1000) {
+        throw std::invalid_argument("too large a number.");
+    }
+    
+    return true;
+}
